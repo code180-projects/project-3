0714dd128729b29b13ab5906ccc995235530bca1
Code180 <42021421+code180-projects@users.noreply.github.com>
2018-11-23 15:13:55 -0700

    Add files via upload

---
+++ my-scripts.js
@@ -0,0 +1,40 @@
+//define html variables
+var computerDisplay = document.getElementById("computerDisplay")
+var resultDisplay = document.getElementById("resultDisplay")
+
+//create user and computer
+var user
+var computer
+
+//create choice prototype
+function Choice(x, y) {
+	this.choice= x;
+	this.beats = y;
+}
+
+//create choices
+var rock = new Choice("rock", "scissors");
+var paper = new Choice("paper", "rock");
+var scissors = new Choice("scissors", "paper");
+
+
+//prepare computer choose function
+var choices = [rock, paper, scissors]
+function computerChoose(x){
+	var index = Math.floor(Math.random() * x.length);
+	computer = x[index];
+}
+
+//user chooses, causing computer to choose
+function userChoose(choice){
+	user = choice;
+	computerChoose(choices);
+	computerDisplay.innerHTML = "Computer chose <b>"+computer.choice+"</b>";
+	if(user.choice == computer.choice){
+		resultDisplay.innerHTML = "Tie";
+	} else if (user.beats == computer.choice) {
+		resultDisplay.innerHTML = "You win!";
+	} else {
+		resultDisplay.innerHTML = "You lose";
+	}
+}
